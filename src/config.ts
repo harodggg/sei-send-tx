bf91aa8a73310e0e764870474a11a68fda6b8d52
harodggg <haroldtiansheng@gmail.com>
2023-03-16 17:25:37 +0800

    fixes

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 import { calculateFee, GasPrice, StdFee } from "@cosmjs/stargate";
 
 export const RPC_ENDPOINT = "https://rpc.atlantic-2.seinetwork.io/";
-export const GAS_PRICE = GasPrice.fromString('100usei');
-export const SEED_FEE: StdFee = calculateFee(100000, GAS_PRICE);
+export const GAS_PRICE = GasPrice.fromString('0.1usei');
+export const SEED_FEE: StdFee = calculateFee(150000, GAS_PRICE);
 export const MNEMONIC = "acquire price raccoon december urge chat setup hat flavor venture stadium barrel";
 export const ADDRESS = "sei1qh7u2jgm04ztdem9y5qxslngx5spl39kpy99ga";
+export const CONTRACT_ADDR = "";
